5277d9ebda383e12c42e507f89d827855d87ee3e
Adele Gikonyo <adele.gikonyo@gmail.com>
2020-08-07 15:26:35 +0300

    begin editable shapes

---
+++ Rectangle.js
@@ -0,0 +1,12 @@
+/* 
+
+1. Plot out a shape a series of vertices
+* Add a button for switching between creating new vertices and editing
+* Click the canvas to add a vertex
+* dont draw right away - add vertex to an array
+2. Edit the vertices using a mouse drag
+3. Confirm the final shape
+
+*/
+
+var editButton

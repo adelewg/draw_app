9897e1ea39554f866f41d8b40d23ad712110ef81
Adele Gikonyo <adele.gikonyo@gmail.com>
2020-08-15 06:51:37 +0300

    added all the code for the custom shape tool but have not yet completely converted it to a function

--- CustomShapeTool.js
+++ CustomShapeTool.js
@@ -19,6 +19,8 @@ function CustomShapeTool() {
         
     var editButton = createButton('Edit Shape');
     var finishButton = createButton('Finish Shape');
+    var editMode = false;
+    var currentShape;
     
     finishButton.mousePressed(function(){
         loadPixels();
@@ -29,20 +31,104 @@ function CustomShapeTool() {
     var currentShape = [];
     
     this.draw = function(){
-        if(mouseIsPressed){
-            currentShape.push({
-                x: mouseX,
-                y: mouseY
-            });
+         updatePixels();
+        if(mousePressOnCanvas(c) && mouseIsPressed){
+            if(!editMode)
+                {
+                    currentShape.push({x:mouseX, y:mouseY});
+                }
+            else
+                {
+                    for(var i = 0; i < currentShape.length; i++)
+                        {
+                            if(dist(currentShape[i].x, currentShape[i].y, mouseX, mouseY) < 15)
+                                {
+                                    currentShape[i].x = mouseX;
+                                    currentShape[i].y = mouseY;
+                                }
+                        }
+                }
+
         }
-        
-        beginShape()
-        for(var i = 0; i < currentShape.length; i++) {
-            vertex(currentShape[i].x, currentShape[i].y);
-        }
-        endShape()
+
+        beginShape();
+        for (var i = 0; i < currentShape.length; i++)
+            {
+                vertex(currentShape[i].x, currentShape[i].y);
+                if(editMode)
+                    {
+                        fill('red');
+                        ellipse(currentShape[i].x, currentShape[i].y, 5,5);
+                        noFill();
+                    }
+            }
+        endShape();
         
         
     }
+    
+    
+    this.mousePressOnCanvas = function(canvas) {
+        if (mouseX > canvas.elt.offsetLeft && 
+            mouseX < (canvas.elt.offsetLeft + canvas.width) && 
+            mouseY > canvas.elt.offsetTop && 
+            mouseY < (canvas.elt.offsetTop + canvas.height))
+            {
+                return true;
+            }
+                return false;
+    }
 
+}
+
+
+var editButton;
+var finishButton;
+
+var editMode = false;
+var currentShape = [];
+
+var c;
+
+function setup()
+{
+	c = createCanvas(800, 700);
+    background(200);
+    noFill();
+    loadPixels();
+    editButton = createButton('Edit Shape');
+    editButton.mousePressed(function(){
+        if(editMode){
+            editMode = false;             
+            editButton.html("Edit Shape");
+        }
+        else{
+            editMode = true;
+            editButton.html("Add Vertices");
+            
+        }
+    })
+    finishButton = createButton("Finish Shape");
+    
+    finishButton.mousePressed(function(){
+        editMode = false;   
+        draw();
+        loadPixels();
+        currentShape = [];
+    })
+}
+
+
+
+
+
+function mousePressOnCanvas(canvas) {
+    if (mouseX > canvas.elt.offsetLeft && 
+        mouseX < (canvas.elt.offsetLeft + canvas.width) && 
+        mouseY > canvas.elt.offsetTop && 
+        mouseY < (canvas.elt.offsetTop + canvas.height))
+        {
+            return true;
+        }
+            return false;
 }
